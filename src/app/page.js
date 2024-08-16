afd57459e762cb305b35f6acffc2daf5863e1dad
Al Amin ET <alaminet.bd@gmail.com>
2024-08-17 00:59:04 +0600

    global-css-text-bg-color-added

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main>
-      <Button className="text-h-1">page</Button>
+      <Button className="text-h-1 text-red bg-white">page</Button>
     </main>
   );
 }
