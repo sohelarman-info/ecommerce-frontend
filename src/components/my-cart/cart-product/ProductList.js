42311f6283ab0866b7e6516ee465505d820b7e73
Anas <anasolin01777@gmail.com>
2024-08-18 14:30:52 +0600

    Cart product part design working

--- src/components/my-cart/cart-product/ProductList.js
+++ src/components/my-cart/cart-product/ProductList.js
@@ -1,5 +1,7 @@
 import { Col, Row } from "react-bootstrap";
 import { productData } from "../data";
+import "../style.css";
+import { FaPlus, FaMinus } from "react-icons/fa6";
 
 const ProductList = () => {
   return (
@@ -7,11 +9,28 @@ const ProductList = () => {
       <Col xs={8}>
         <div>
           {productData.map((item, i) => (
-            <div className="d-flex" key={i}>
-              <input type="radio" />
-              <img src={item.image} className="box-56" alt="Product Image" />
-              <h2>{item.name}</h2>
-              <h2>{item.price}</h2>
+            <div className="d-flex align-items-center" key={i}>
+              <input type="checkbox" className="text-red" />
+              <img
+                src={item.image}
+                className="box-180 mx-5"
+                alt="Product Image"
+              />
+              <div>
+                <h5>{item.name}</h5>
+                <h5 className="text-red">{item.price}</h5>
+                <div className="d-flex ">
+                  <div className="counter-btn counter-gray-btn text-gray d-flex justify-content-center align-items-center">
+                    <FaMinus />
+                  </div>
+                  <span className="text-red px-3 text-center align-content-center">
+                    1
+                  </span>
+                  <div className="counter-btn text-white  bg-red d-flex justify-content-center align-items-center">
+                    <FaPlus />
+                  </div>
+                </div>
+              </div>
             </div>
           ))}
         </div>
