b6efc55cd820210687815a9b1afe36d7a83bec18
mdhadiul0011 <mdhadiul0011@gmail.com>
2024-08-21 20:13:35 +0600

    comment-part

--- src/component/productpart/reivewformpart.js
+++ src/component/productpart/reivewformpart.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Reivewformpart() {
   return (
-    <div>
-      hello review form part
+    <div className='review-form-part'> 
+      hello review form part amar sonar bangla ami tomay valo basi..
     </div>
   )
 }
--- src/component/productpart/reviewpart.js
+++ src/component/productpart/reviewpart.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Images from 'next/image'
+import { commentdata } from './productdata'
 
 function Reviewpart() {
   return (
@@ -9,6 +11,33 @@ function Reviewpart() {
             <p>Discusion</p>
             <p>Gift Cards</p>
         </div>
+        <div className='comment-list' >
+            {
+                commentdata.map((item, i)=>(
+                    <div className='tag-comment' key={i}>
+                        <div className='cmnt-element'>
+                            <div className='cmnt-img-rate'>
+                                <div className='img'>
+                                    <Images src={item.img} width={56} height={56} alt='comment-img'/>
+                                </div>
+                                <div className='review-text'>
+                                    <h4>{item.cmntname}</h4>
+                                    <div className='rating'>
+                                        <p>{item.reviewrate}</p>
+                                        <Images src={item.cmntrateimg} width={100} height={20} alt='review'/>
+                                        <p className='rate-timing'>{item.time}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                            <p className='cmnt'>
+                                {item.cmnt} 
+                            </p>
+                    </div>
+                ))
+            }
+        </div>
+
     </div>
   )
 }
