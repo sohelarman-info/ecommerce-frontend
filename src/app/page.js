c6c5f0d11ffebed76c2d6a9c324ba376ba9af193
Sohel Arman <84041259+sohelarman-info@users.noreply.github.com>
2024-08-22 22:54:29 +0600

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import { Button, Stack } from "react-bootstrap";
 import styles from "./page.module.css";
+import Hero from "@/sections/hero";
 
 export default function Home() {
   return (
     <main>
       <Button as="a" variant="primary">
-        page
+        <Hero />
       </Button>
     </main>
   );
